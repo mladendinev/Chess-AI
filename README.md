5f23477ac88a485dc47d062f021f79502db49337
Mladen Dinev <mladen@primer.com>
2023-03-19 14:19:08 +0200

    Readme file and cleanup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Chess-AI
+
+Workflow:
+
+1. Download chess database, preferably with games played by high ranked chess players.
+2. Parse the game boards
+3. Encode the information from the board
+4. Configure an appropriate Neural Network
+5. Train the NN model
+6. Use a value function returning the best possible move in the current state of the game.
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import chess
+import chess.svg
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtWidgets import QApplication, QWidget
 
+class MainWindow(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.setGeometry(100, 100, 1100, 1100)
+        self.widgetSvg = QSvgWidget(parent=self)
+        self.widgetSvg.setGeometry(10, 10, 1080, 1080)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+        board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
+        self.chessboardSvg = chess.svg.board(
+            board,
+            fill=dict.fromkeys(board.attacks(chess.E4), "#cc0000cc"),
+            arrows=[chess.svg.Arrow(chess.E4, chess.F6)],
+            squares=chess.SquareSet(chess.BB_DARK_SQUARES & chess.BB_FILE_B),
+            size=350,
+        ).encode("UTF-8")
+        
+        self.widgetSvg.load(self.chessboardSvg)
 
-
-# Press the green button in the gutter to run the script.
+    
+    
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
