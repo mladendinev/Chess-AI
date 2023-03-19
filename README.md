6ec527c71152ab12d1389895794d350a3d260204
Mladen Dinev <mladen@primer.com>
2023-03-19 15:08:12 +0200

    playable game with gui

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Chess-AI
 
-Workflow:
+# Workflow:
 
 1. Download chess database, preferably with games played by high ranked chess players.
 2. Parse the game boards
@@ -8,3 +8,36 @@ Workflow:
 4. Configure an appropriate Neural Network
 5. Train the NN model
 6. Use a value function returning the best possible move in the current state of the game.
+
+# Notes:
+
+The position of the piece on the board is represented in binary
+
+Board is represetned as - 8x8 - 64 bits
+
+=====WHITE=====
+Pawn - 1
+Knight - 2
+Bishop - 3
+Rook - 4
+Queen - 5
+King - 6
+
+=====BLACK=====
+Pawn - 9
+Knight - 10
+Bishop - 11
+Rook - 12
+Queen - 13
+King - 14
+
+All the white pieces have their left most bit as 0 - (0001), (0010), (0011), (0100), (0101), (0110)
+All black pieces have their left most bit as 1 - (1001), (1010), (1011), (1100), (1101), (1110)
+
+The other three bits indicate the piece type independent of the colour.
+
+The left most bit identifies the colour of the piece - if 1 then black, else white. "&" is the AND bitwise operator - 1&1 == true(1) 0&1 == false (0)
+
+===== AI and Neural networks
+
+np.zeros((5,8,8), np.unint8) -> 5 matrices each 8x8 representing 5 different neurons for each field on the board
--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import chess
 import chess.svg
-
+import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWidgets import QApplication, QWidget
+
 
 class MainWindow(QWidget):
     def __init__(self) -> None:
         super().__init__()
-        
+
+        self.modified = False
         self.setGeometry(100, 100, 1100, 1100)
         self.widgetSvg = QSvgWidget(parent=self)
         self.widgetSvg.setGeometry(10, 10, 1080, 1080)
 
-        board = chess.Board("8/8/8/8/4N3/8/8/8 w - - 0 1")
-        self.chessboardSvg = chess.svg.board(
-            board,
-            fill=dict.fromkeys(board.attacks(chess.E4), "#cc0000cc"),
-            arrows=[chess.svg.Arrow(chess.E4, chess.F6)],
-            squares=chess.SquareSet(chess.BB_DARK_SQUARES & chess.BB_FILE_B),
-            size=350,
-        ).encode("UTF-8")
-        
+        self.board = chess.Board()
+        self.chessboardSvg = chess.svg.board(self.board).encode("UTF-8")
         self.widgetSvg.load(self.chessboardSvg)
 
-    
-    
+    def paintEvent(self, event):
+        if self.modified:
+            self.chessboardSvg = chess.svg.board(self.board).encode("UTF-8")
+            self.widgetSvg.load(self.chessboardSvg)
+            self.widgetSvg.load(self.chessboardSvg)
+            self.modified = False
+
+    def keyPressEvent(self, event):
+        gey = event.key()
+        if gey == Qt.Key_D:
+            sys.exit()
+
+
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
     window.show()
-    app.exec()
+    while True:
+        text = input("Next move \n")
+        print(list(window.board.legal_moves))
+
+        if (text):
+            move = chess.Move.from_uci(text)
+            window.board.push(move)
+            window.modified = True
+            window.update()
